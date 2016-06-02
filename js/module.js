42f14a748db6f936822f637f2edb64146823de7e
Josh Houck <Josh123090@gmail.com>
2016-06-02 15:11:40 -0400

    change projects to view

--- js/module.js
+++ js/module.js
@@ -5,13 +5,25 @@ var app = angular.module('webApp', ['ui.router', 'ui.materialize', 'ngAnimate'])
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$stateProvider
-	.state('about', {
-		url: '/',
-		templateUrl: 'html/about.html'
+	.state('flashcards', {
+		url: '/flashcards',
+		templateUrl: 'html/flashcards.html'
 	})
-	.state('projects', {
-		url: '/projects',
-		templateUrl: 'html/projects.html'
+	.state('drawSomething', {
+		url: '/drawSomething',
+		templateUrl: 'html/drawSomething.html'
 	})
-	$urlRouterProvider.otherwise('/');
+	.state('countGame', {
+		url: '/countGame',
+		templateUrl: 'html/countGame.html'
+	})
+	.state('foodle', {
+		url: '/foodle',
+		templateUrl: 'html/foodle.html'
+	})
+	.state('car2Hot', {
+		url: '/car2Hot',
+		templateUrl: 'html/car2Hot.html'
+	})
+	$urlRouterProvider.otherwise('/flashcards');
 });
